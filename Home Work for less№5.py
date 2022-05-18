dcc6d8d0881883c3793288e2dc0138b5610e53fe
Vyacheslav <9366086@inbox.ru>
2022-05-18 12:48:03 +0300

    задача № 4

--- Home Work for less№5.py
+++ Home Work for less№5.py
@@ -1,6 +1,100 @@
 # 1. Напишите программу, удаляющую из текста все слова, содержащие "абв".
 
-my_str = 'Напишите прогабврамму удабваляющую из текста все слова содержащие'
-my_str = my_str.split()
-res = [word for word in my_str if word.find('абв') == -1]
-print(res)
+# my_str = 'Напишите прогабврамму удабваляющую из текста все слова содержащие'
+# my_str = my_str.split()
+# res = [word for word in my_str if word.find('абв') == -1]
+# print(res)
+
+#***************************************************************************************
+
+# 2. Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета. 
+# Играют два игрока делая ход друг после друга. 
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+#  Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, 
+#  чтобы забрать все конфеты у своего конкурента?
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота "интеллектом"
+
+# НЕ УСПЕЛ  !!!!!!
+
+#**************************************************************************************
+#3. Создайте программу для игры в "Крестики-нолики".
+# board = list(range(1,10))
+
+# def draw_board(board):
+#     print ("-" * 13)
+#     for i in range(3):
+#         print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+#         print ("-" * 13)
+
+# def take_input(player_token):
+#     valid = False
+#     while not valid:
+#         player_answer = input("Куда поставим " + player_token+"? ")
+#         try:
+#             player_answer = int(player_answer)
+#         except:
+#             print ("Некорректный ввод. Вы уверены, что ввели число?")
+#             continue
+#         if player_answer >= 1 and player_answer <= 9:
+#             if (str(board[player_answer-1]) not in "XO"):
+#                 board[player_answer-1] = player_token
+#                 valid = True
+#             else:
+#                 print ("Эта клеточка уже занята")
+#         else:
+#             print ("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
+
+# def check_win(board):
+#     win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+#     for each in win_coord:
+#         if board[each[0]] == board[each[1]] == board[each[2]]:
+#             return board[each[0]]
+#     return False
+
+# def main(board):
+#     counter = 0
+#     win = False
+#     while not win:
+#         draw_board(board)
+#         if counter % 2 == 0:
+#             take_input("X")
+#         else:
+#             take_input("O")
+#         counter += 1
+#         if counter > 4:
+#             tmp = check_win(board)
+#             if tmp:
+#                 print (tmp, "выиграл!")
+#                 win = True
+#                 break
+#         if counter == 9:
+#             print ("Ничья!")
+#             break
+#     draw_board(board)
+
+# main(board)
+
+#***********************************************************************
+# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+
+import sys
+
+sys.stdout = open('text_2.txt','w')
+with open("text_1.txt", "r") as f:
+    string = f.read()
+count = 1
+for i in range(len(string)-1):
+    if i <= len(string):
+        if string[i] == string[i + 1]:
+            count += 1
+        else:
+            a = string[i]
+            print(count, string[i])
+            count = 1         
+print(count, string[i])
+sys.stdout.close() #наверно я никогда не стану программистом(((
+                   #только на эту задачу потратил 4 часа !!!!
